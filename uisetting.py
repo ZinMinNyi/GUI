05cec98e6c8febc933d06ae15eb2d80079ec6b89
ZinMinNyi <58581822+ZinMinNyi@users.noreply.github.com>
2020-08-07 17:31:20 +0630

    Add files via upload

--- uisetting.py
+++ uisetting.py
@@ -18,22 +18,22 @@ def click_1():
 
 setFrame = Frame(root, bg = bg, bd = 0, width = 800, height = 800, relief = "groove")
 setFrame.config(highlightcolor = color, highlightbackground = color, highlightthickness = 3)
-setFrame.pack(anchor = "center", pady = 200)
+setFrame.pack(anchor = "center", pady = 100)
 
-setLabel = Label(setFrame, text = "Setting", font = ('times',24,'bold', 'italic', 'underline'), padx = 150, pady = 20, bg = bg, fg = color)
-setLabel.grid(row = 0, column = 1)
+setLabel = Label(setFrame, text = "Setting", font = ('times',24,'bold', 'italic'), padx = 150, pady = 20, bg = bg, fg = color)
+setLabel.grid(row = 0, column = 1,columnspan=3)
 
-modLabel = Label(setFrame, text = "Mode", font = ('times',14,'bold', 'italic'), bg = bg, fg = color)
-modLabel.grid(row = 5, column = 0)
+modLabel = Label(setFrame, text = "Mode", font = ('times',14,'bold'), bg = bg, fg = color)
+modLabel.grid(row = 1, column =1)
 
 radio_button = IntVar()
 
 rb1 = ttk.Radiobutton(setFrame, text = "Light Mode", variable = radio_button, value = 1, command = click_1)
-rb1.grid(row = 5, column = 1, columnspan = 1, padx = 30, pady = 20)
+rb1.grid(row = 1, column = 2,sticky="e")
 
 rb2 = ttk.Radiobutton(setFrame, text = "Dark Mode", variable = radio_button, value = 2, command = click_1, style = "Wild.TRadiobutton")
-sty2 = ttk.Style()
-sty2.configure("Wild.TRadiobutton", background = bg, foreground = wbg)
-rb2.grid( row = 5, column = 2, columnspan = 2, pady = 20)
+sty = ttk.Style()
+sty.configure("Wild.TRadiobutton", background = bg, foreground = wbg)
+rb2.grid( row = 1, column = 3,sticky="w")
 
 root.mainloop()
