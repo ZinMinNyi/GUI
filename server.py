1bf04e39f9bd20e1ed5c80750915354531b2fe9a
ZinMinNyi <58581822+ZinMinNyi@users.noreply.github.com>
2020-08-12 23:03:58 +0630

    update server

--- server.py
+++ server.py
@@ -33,76 +33,70 @@ def decrypt(enc, password):
 def acceptIncomingConnection():
     while True:
         client , clientAddr = server.accept()
-        print("%s : %s has connected." % clientAddr)
+        print("%s : %s has connected."% clientAddr)
         addresses[client] = clientAddr
         # print(client) # the whole data of connection
         # print(addresses[client]) # client IP addr
-        Thread(target = handleClient, args = (client,)).start() # Create a thread for each client
+        Thread(target=handleClient,args=(client,)).start() # Create a thread for each client
 def handleClient(client):
     while True:
-        connetionStart = bytes.decode(decrypt(client.recv(bufsiz), password))
+        connetionStart = bytes.decode(decrypt(client.recv(bufsiz),password))
         data = json.loads(connetionStart)
         # to signup
         if data["to"] == "signup":
             cur.execute(dbQuery["checkUsername"].format(data["username"]))
             checkUsername = cur.fetchall()
-
             #check wherethere username is avaliable 
             if checkUsername:
-                validReply = {"to":"valid", "msg":"Username already exists!"}
+                validReply = {"to":"valid","msg":"Username already exists!"}
                 validReply = json.dumps(validReply)
-                client.send(encrypt(validReply, password))
-
+                client.send(encrypt(validReply,password))
             #if it is avaliable insert it into database
             else :
-                cur.execute(dbQuery["signup"].format(data["username"], data["email"], data["ps"]))
+                cur.execute(dbQuery["signup"].format(data["username"],data["email"],data["ps"]))
                 con.commit()
-
         # to login
         elif data["to"] == "login":
-            cur.execute(dbQuery["login"].format(data["username"], data["ps"]))
+            cur.execute(dbQuery["login"].format(data["username"],data["ps"]))
             login = cur.fetchall()
-
             # check wherethere username or password is correct
             if not login:
-                loginReply = {"to":"loginReply", "msg":"Wrong Username or Password!", "access":0}
+                loginReply = {"to":"loginReply","msg":"Wrong Username or Password!","access":0}
                 loginReply = json.dumps(loginReply)
-                client.send(encrypt(loginReply, password))
-
+                client.send(encrypt(loginReply,password))
             # if correct, we put them in a dict to kepp track of who is online
             else :
-                clients[client] = [login[0][0], data["username"]]
-                loginAccessReply = {"to":"loginAccessReply", "msg":"Access Granted!", "access":1}
+                clients[client] = [login[0][0],data["username"]]
+                loginAccessReply = {"to":"loginAccessReply","msg":"Access Granted!","access":1}
                 loginAccessReply = json.dumps(loginAccessReply)
-                client.send(encrypt(loginAccessReply, password))
+                client.send(encrypt(loginAccessReply,password))
         # to AI
         elif data["to"] =="helpCenter":
             pass
-
+        # Client Disconnect 
+        elif data["to"] =="//clientDisconnect":
+            client.close()
+            del clients[client]
+            break
         # Chat
         else : 
-            pass
-
+            for onlineList in clients:
+                if clients[onlineList][1]==data["to"]:
+                    onlineList.send(encrypt(json.dumps(data),password))
+                else :
+                    offline = {"to":"offline","msg":"This user is offline"}
+                    offline = json.dumps(offline)
+                    client.send(encrypt(offline,password))
 # Send out messages
-def broadCast(msg = "", type = ""):
+def broadCast(msg="",type=""):
     pass
 
 clients = {}
 addresses = {}
 # Database Query 
-dbQuery = {"userinfo": """create table userinfo (id int primary key not null AUTO_INCREMENT, username varchar(100) not null, email varchar(100) not null, password varchar(50) not null);""",
-           "signup" : """insert into userinfo (username, email, password) values ("{}", "{}", "{}");""", "checkUsername":"""select username from userinfo where username = "{}";""",
-           "login" : """select id from userinfo where username = "{}" and password = "{}";""" }
-
-# Server Info
-host = ""
-port = 33000
-bufsiz = 1024
-addr = (host, port)
-
-# Create socket and bind IP and port
-server = socket(AF_INET, SOCK_STREAM)
-server.bind(addr)
+dbQuery = {"userinfo":"""create table userinfo (id int primary key not null AUTO_INCREMENT, username varchar(100) not null, email varchar(100) not null, password varchar(50) not null);""",
+           "signup" : """insert into userinfo (username,email,password) values ("{}","{}","{}");""","checkUsername":"""select username from userinfo where username="{}";""",
+           "login" : """select id from userinfo where username="{}" and password="{}";""" ,"createFriendList":"""create table friendlist (id int primary key AUTO_INCREMENT,friendls varchar(150));"""}
 
 # Start *
 # Note -- We use MySQL
@@ -111,19 +105,16 @@ while True:
     user = input("Enter Database username : ")
     dbpassword = input("Enter Password : ")
     try : 
-        con = mc.connect(host = "localhost", user = user, password = dbpassword)
+        con = mc.connect(host="localhost",user=user,password=dbpassword)
         os.system('cls')
         break
-    
     except :
         os.system('cls')
         print("Wrong Username or Password!")
-
 # Create a Cursor 
 cur = con.cursor()
 cur.execute("show databases")
 alldb = cur.fetchall()
-
 # Checking wherethere needed database exist if not we will create 
 if ("livechat",) not in alldb:
     cur.execute("create database livechat")
@@ -131,12 +122,21 @@ if ("livechat",) not in alldb:
     cur.execute(dbQuery["userinfo"])
 cur.execute("use livechat")
 
+# Server Info
+host = ""
+port = 33000
+bufsiz = 1024
+addr = (host,port)
+# Create socket and bind IP and port
+server = socket(AF_INET,SOCK_STREAM)
+server.bind(addr)
+
 # Get Server IP
-print("Server IP : " + gethostbyname(gethostname()))
+print("Server IP : "+gethostbyname(gethostname()))
 
 if __name__ == "__main__":
     server.listen(5)
-    acceptThread = Thread(target = acceptIncomingConnection)
+    acceptThread = Thread(target=acceptIncomingConnection)
     acceptThread.start()
     acceptThread.join()
     server.close()
